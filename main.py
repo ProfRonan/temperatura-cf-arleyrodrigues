f267bddba29663a1db0359a625dffb6e410638cd
arleyrodrigues <arleyrb@alu.ufc.br>
2020-12-11 17:20:15 +0000

    Por fim, esse também esqueci o ponto final.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     # COLOQUE SEU CÓDIGO AQUI
     c = float(input("Quala temperatura em graus Celsius?\n"))
     j = c *9/5 + 32
-    print(f'A temperatura em Fahrenheit é {j}')
+    print(f'A temperatura em Fahrenheit é {j}.')
 
 
 if __name__ == '__main__':
